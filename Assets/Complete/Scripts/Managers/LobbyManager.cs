e9e245c387da3a48f881849d8887e46a2d796724
Scnoobi <scnoobers@gmail.com>
2015-10-26 20:14:04 +0100

    Trying to figure out how to add players. think i have figured it out. but have yet to write much of it inside the code. ONWARDS TO TOMMOROW!

--- Assets/Complete/Scripts/Managers/LobbyManager.cs
+++ Assets/Complete/Scripts/Managers/LobbyManager.cs
@@ -7,6 +7,22 @@ namespace Complete
     public class LobbyManager : MonoBehaviour
     {
         List<int> meow = new List<int>();
+        private int number = 0;
+        public GameObject playerPrefab;
+        private List<int> playersNotInstantiatedNumbers = new List<int>();
+        public int amountOfPlayers = 4;
+
+        public static LobbyManager instance;
+
+        void Awake()
+        {
+            instance = this;
+            for (int i = 0; i < amountOfPlayers; i++)
+            {
+                playersNotInstantiatedNumbers.Add(i);
+            }
+            
+        }
         void Update()
         {
             //Everyone is ready. and some1 pressed start.
@@ -16,7 +32,38 @@ namespace Complete
             //    GameManager.instance.StartGame();
 
             //If a new players joins add them to the list
-            meow.Add(meow.Count - 1);
+
+                CheckForPlayerInput();
+        }
+        void CheckForPlayerInput()
+        {
+            foreach (var item in playersNotInstantiatedNumbers)
+            {
+                if (Input.GetButtonDown("fire" + item))
+                {
+                    var clone = Instantiate(playerPrefab);
+
+                    clone.GetComponent<Player>().JustGotInstantiated(number);
+                    playersNotInstantiatedNumbers.Remove(item);
+                }
+            }
+            //if(Input.GetButtonDown("fire" + number))
+            //{
+                
+            //    var clone = Instantiate(playerPrefab);
+
+            //    clone.GetComponent<Player>().JustGotInstantiated(number);
+
+            //    number++;
+            //    //TODO Add a player with their number. so you can diffenriate what they do.
+            //}
+        }
+
+        public void APlayerLeft()
+        {
+            //TODO
+            //If a player leaves. reduce the number
+            //If a player left. then delete that playerprefab.
         }
     }
 }
---
+++ Assets/Complete/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Complete
+{
+    public class Player : MonoBehaviour
+    {
+
+        private int myNumber;
+
+
+        public void JustGotInstantiated(int myPlayerNumber)
+        {
+            myNumber = myPlayerNumber;
+        }
+
+        void Update()
+        {
+            //If a player presses the leave button then say that to lobbymanager
+            if(Input.GetButtonDown("leave" + myNumber))
+            {
+                LobbyManager.instance.APlayerLeft();
+            }
+        }
+    }
+}
